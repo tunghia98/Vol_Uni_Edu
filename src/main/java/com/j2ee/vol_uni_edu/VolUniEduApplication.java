edb01c15e9b1aa0fcb98f74d1bd0e77001456bea
Nguyễn Tư Nghĩa <tunghia98@gmail.com>
2024-10-13 01:32:53 +0700

    first commit

---
+++ src/main/java/com/j2ee/vol_uni_edu/VolUniEduApplication.java
@@ -0,0 +1,13 @@
+package com.j2ee.vol_uni_edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolUniEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolUniEduApplication.class, args);
+    }
+
+}
